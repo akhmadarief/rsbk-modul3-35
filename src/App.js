2e9918be4c712fb8ed245b543aeebf917624ea96
akhmadarief <38371717+akhmadarief@users.noreply.github.com>
2020-11-22 20:40:50 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Parent from "./modul3/Parent";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Oyakodon</p>
+        <Parent />
+      </div>
+    )
+  }
+}
+
+export default App;
